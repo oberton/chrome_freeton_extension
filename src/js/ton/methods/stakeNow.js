f43853a670595a4c6bd943408f8e42eba6071fc6
Oleh Hudeichuk <emptystamp@gmail.com>
2021-03-26 00:52:36 +0200

    temp deploy

--- src/js/ton/methods/stakeNow.js
+++ src/js/ton/methods/stakeNow.js
@@ -383,6 +383,120 @@ async function sendToken(client, from, to, amount, keys, abiWalletDir, sendForce
 
 
 
+// amount - размерность в TON
+async function deployStandardWalletContract(keys, abiDir, tvcDir, owners, expectedAddress = null, reqConfirms = 0) {
+  const client = conf.tonClient;
+  
+  // Account is active when contract is deployed.
+  const ACCOUNT_TYPE_ACTIVE = 1;
+
+  // Account is uninitialized when contract is not deployed yet.
+  const ACCOUNT_TYPE_UNINITIALIZED = 0;
+
+  // Number of tokens required to deploy the contract.
+  const CONTRACT_REQUIRED_DEPLOY_TOKENS = 500_000_000;
+
+  const abiValue = await fetchAbi(abiDir);
+  const tvcValue = await fetchTvc(tvcDir);
+
+  const { address } = await client.abi.encode_message({
+    abi: {
+        type: 'Contract',
+        value: abiValue
+    },
+    deploy_set: {
+        tvc: tvcDir,
+        initial_data: {}
+    },
+    call_set: {
+        function_name: 'constructor',
+        input: {
+            owners: owners,
+            reqConfirms: reqConfirms
+        }
+    },
+    signer: {
+        type: 'Keys',
+        keys: keys
+    },
+    processing_try_index: 1
+  });
+
+  if (expectedAddress != null && expectedAddress != address) {
+    throw new Error(`Expected Address: ${expectedAddress} but get ${address}`)
+  }
+
+  const { result } = await client.net.query_collection({
+    collection: 'accounts',
+    filter: {
+      id: {
+        eq: address
+      }
+    },
+    result: 'acc_type balance code'
+  });
+
+  if (result.length === 0) {
+    const mess = `You need to transfer at least 0.5 tokens for deploy to ${address} to your network`
+    console.log(mess);
+    throw new Error(mess)
+  }
+
+  if (result[0].acc_type == ACCOUNT_TYPE_ACTIVE) {
+    const mess = `Contract is already deployed to ${address}`
+    console.log(mess);
+    throw new Error(mess)
+  }
+
+  // Balance is stored as HEX so we need to convert it.
+  if (result[0].acc_type == ACCOUNT_TYPE_UNINITIALIZED && BigInt(result[0].balance) <= BigInt(CONTRACT_REQUIRED_DEPLOY_TOKENS)) {
+    const mess = `Balance of ${address} is too low for deploy to network`
+    console.log(mess);
+    throw new Error(mess)
+  }
+
+  const response = await client.processing.process_message({
+    send_events: false,
+    message_encode_params: {
+      abi: {
+        type: 'Contract',
+        value: abiValue
+      },
+      deploy_set: {
+        tvc: tvcValue,
+        initial_data: {}
+      },
+      call_set: {
+        function_name: 'constructor',
+        input: {
+          owners: owners,
+          reqConfirms: reqConfirms
+        }
+      },
+      signer: {
+          type: 'Keys',
+          keys: keys
+      },
+      processing_try_index: 1
+    }
+  });
+  console.log(`Transaction id is ${response.transaction.id}`);
+  console.log(`Deploy fees are  ${JSON.stringify(response.fees, null, 2)}`);
+  console.log(`Contract is successfully deployed. You can play with your multisig wallet now at ${address}`);
+
+  return response;
+}
+
+async function deploySurfWallet(keys, abiDir, tvcDir, expectedAddress) {
+  console.log(keys);
+  return await deployStandardWalletContract(keys, abiDir, tvcDir, [`0x${keys.public}`], expectedAddress);
+}
+
+async function deploySafeMultisigWallet(keys, abiDir, tvcDir, owners, expectedAddress, reqConfirms) {
+  return await deployStandardWalletContract(keys, abiDir, tvcDir, owners, expectedAddress);
+}
+
+
 
 
 
@@ -437,19 +551,27 @@ async function stakeNow(walletAddr, keys, depoolAddr, abiDepoolDir, abiWalletDir
   // console.log(a);
 
   // отправить токены
-  let a = await sendToken(
-    client, 
-    walletAddr, 
-    // '0:122f9c193e2b925e432356ef1e868ee84559696a48845a8b9cf781c67f2c75d9', 
-    '0:6b824a6cc6e879e584bfd88025677c51ac560f2614b7e87d60dc9c8ec884dc96', 
-    0.1, 
-    keys, 
-    abiWalletDir,
-    false, 
-    null
-  );
-  console.log(a);
+  // let a = await sendToken(
+  //   client, 
+  //   walletAddr, 
+  //   // '0:122f9c193e2b925e432356ef1e868ee84559696a48845a8b9cf781c67f2c75d9', 
+  //   '0:6b824a6cc6e879e584bfd88025677c51ac560f2614b7e87d60dc9c8ec884dc96', 
+  //   0.1, 
+  //   keys, 
+  //   abiWalletDir,
+  //   false, 
+  //   null
+  // );
+  // console.log(a);
 
+
+
+  let testNewSetcodeWalletAddress = '0:f851ca051b33d869f0ebf63f0c7f9c7487610298bcbbfd01813c39f65ae0ff43'
+  let setcodeTVCDir = '/sig-files/SetcodeMultisigWallet2.tvc'
+  let abit = await fetchTvc(setcodeTVCDir);
+
+  // let r = await deploySurfWallet(keys, abiWalletDir, tvcDir, expectedAddress)
+  console.log(r);
 
 
   return;
